a9f48c53139ea464db79d09e5326369c624e22cd
maxfrolov-cloud <max.frolov@voco.ee>
2026-09-08 10:47:21 +0000

    komponentide kompositsioon ja info edastamine

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,13 +1,14 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem.jsx";
+import Card from './Card';
 
 const Expenses = (props) => {
   console.log(props.data[0]);
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem data={props.data[0]} />
       <ExpenseItem data={props.data[1]} />
-    </div>
+    </Card>
   );
 };
 
