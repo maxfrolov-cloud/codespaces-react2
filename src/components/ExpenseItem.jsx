27fcfa68c9ddb5ca59284ba6087a02fbb03fc88d
maxfrolov-cloud <max.frolov@voco.ee>
2026-09-09 07:35:52 +0000

    Ülesanne 3 - ExpenseItem ümber kujundamine olemasoleva komponendi abil

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,17 @@
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
+import Card from './Card.jsx'
 
 const ExpenseItem = (props) => {
 
     return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
         <ExpenseDate date={props.data.date}/>
         <div className='expense-item__description'>
             <h2>{props.data.title}</h2>
             <div className='expense-item__price'>{props.data.price}</div>
         </div>
-    </div>
+    </Card>
     )
 }
 export default ExpenseItem
