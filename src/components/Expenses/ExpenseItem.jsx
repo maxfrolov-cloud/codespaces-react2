388dd5fcd5d8c3418afb9fcdd44979c7dedffb60
maxfrolov-cloud <max.frolov@voco.ee>
2026-09-14 08:41:41 +0000

    10. Sündmuste töötlus komponentides

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,8 +1,15 @@
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 import Card from "../UI/Card";
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.data.title)
+    const clickHandler = () => {
+        console.log('Clicked')
+        setTitle('Updated by click ${title}')
+        console.log(title)
+    }
 
     return (
     <Card className='expense-item'>
@@ -11,6 +18,7 @@ const ExpenseItem = (props) => {
             <h2>{props.data.title}</h2>
             <div className='expense-item__price'>{props.data.price}</div>
         </div>
+        <button onClick={clickHandler}>Click Me</button>
     </Card>
     )
 }
