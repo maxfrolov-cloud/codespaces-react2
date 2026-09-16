8be5c75a2455919b96065f805c396d11f93e34e2
maxfrolov-cloud <max.frolov@voco.ee>
2026-09-16 06:16:30 +0000

    11.Formi loomine ja kujundamine

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
+ 
 function App() {
 
   const expenses =[
@@ -19,6 +21,7 @@ function App() {
 
   return (
     <div className="App">
+    <NewExpense></NewExpense>
       <Expenses data={expenses}/>
     </div>
   );
